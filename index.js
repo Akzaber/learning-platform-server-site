6003960ba067a7895666ee41865d204032f6845c
Akzaber <zaberkhan611@gmail.com>
2022-10-25 21:34:07 +0600

    categories data loaded

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 5000;
+
+app.use(cors());
+
+const categories = require("./Data/Category.json");
 
 app.get("/", (req, res) => {
   res.send("Hello Server Courses");
+});
+
+app.get("/courses-category", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
